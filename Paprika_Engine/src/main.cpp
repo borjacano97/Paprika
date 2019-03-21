5aefef8b670ca09928e94e91cb32cb9da3346f84
Borja Cano <borcano@ucm.es>
2019-03-21 20:47:32 +0100

    SDL2 dependence setup

--- Paprika_Engine/src/main.cpp
+++ Paprika_Engine/src/main.cpp
@@ -1,8 +1,24 @@
+#define SDL_MAIN_HANDLED
 #include <stdio.h>
+#include <SDL.h>
 
 int main(int argc, char** argv) 
 {
-	printf("Hello World!\nI'm the Paprika engine\n");
-	getchar();
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* win = SDL_CreateWindow("Paprika engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Surface* surface = SDL_GetWindowSurface(win);
+	int color = ~0u;
+	bool close = false;
+	while (!close)
+	{
+		SDL_FillRect(surface, NULL, --color);
+		SDL_UpdateWindowSurface(win);
+		static SDL_Event e;
+		while (SDL_PollEvent(&e) && !(close = e.type == SDL_QUIT));
+	}
+
+
+	SDL_Quit();
 	return 0;
 }
